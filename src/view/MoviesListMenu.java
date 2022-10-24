3357f0960730c8d542201d12ddaa9ef8ce7475b1
AloysiusTan <97999822+AloysiusTan@users.noreply.github.com>
2022-10-24 15:36:36 +0800

    Added MovieGoerMainMenu, MovieInfo, MovieSearchMenu, MoviesListMenu, PurchaseTicketMenu. Added some helper functions in utilF.

---
+++ src/view/MoviesListMenu.java
@@ -0,0 +1,76 @@
+package view;
+
+import java.util.*;
+import static view.utilF.*;
+public class MoviesListMenu extends MenuBase {
+
+    public MoviesListMenu(MenuBase previousMenu) {
+        super(previousMenu);
+    }
+
+    /*
+     Display all movies, Ask user to choice the next menu and bring user to next stage of application
+     Consist of four options
+     1. Show Top 5 movies by sales
+     2. Show Top 5 movies by rating
+     3. Back to previous menu
+     4. Quit Booking System
+     */
+
+    public MenuBase execute() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Movie Listing");
+        Manager mg = Manager.getInstance(); //get a manager object to access the database
+
+        ArrayList<String> choices = new ArrayList<String>();
+        //.getAll() is to retrieve all the movies
+        ArrayList<Movie> movies = mg.getAll();
+
+        //list all the movies out
+        for (Movie m : movies) {
+            //get the current time
+            Date currentTime = Calendar.getInstance().getTime();
+            //.getEnding is to retrieve the last showing date
+            if(m.getEnding().compareTo(currentTime) < 0) { //not showing anymore
+                m.setShowingStatus(Constant.ShowingStatus.NOT_SHOWING); //set the status to NOT SHOWING
+            }
+            else if (m.getEnding().compareTO(currentTime) >= 0) { //currently showing
+                m.setShowingStatus(Constant.ShowingStatus.CURRENTLY_SHOWING);
+            }
+            String title = m.getTitle() + (m.getShowingStatus().equals(Constant.ShowingStatus.END_SHOWING) ? " (End Showing)" : "");
+            choices.add(title); //add the movie as part of the option
+        }
+
+        choices.add("Show Top 5 Movies by sale");
+        choices.add("Shop Top 5 Movies by ratings");
+        choices.add("Go Back");
+        choices.add("Quit The Application");
+        printMenu(choices, 1);
+
+        /* eg. movies.size == 2
+           Option 1: Movie 1
+           Option 2: Movie 2
+           Option 3: Show Top 5 Movies by sale
+           Option 4: Shop Top 5 Movies by ratings
+           Option 5: Go Back
+           Option 6: Quit The Application
+         */
+        String choice = sc.next();
+        int c = readIntInput(choice);
+
+        MenuBase nextMenu = this;
+        if (c <= movies.size()) //Selecting 1 of the movies
+            nextMenu = new MovieInfo(this, movies.get(c));
+        else if (c == movies.size() + 1) //Select Top5 Movies by sale
+            nextMenu = new MovieListTopSale(this.getPreviousMenu());
+        else if (c == movies.size() + 2) //Select Top5 Movies by rating
+            nextMenu = new MoviesListTopRatings(this.getPreviousMenu());
+        else if (c == movies.size() + 3) //GO back to the previous menu
+            nextMenu = this.getPreviousMenu();
+        else if (c == movies.size() + 4) //Quit the App
+            nextMenu = new Quit(null);
+
+        return nextMenu;
+    }
+
+}
--- src/view/utilF.java
+++ src/view/utilF.java
@@ -1,13 +1,12 @@
 package view;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /* Functions to help us to process printing/reading inputs */
 public class utilF {
     private static Scanner sc = new Scanner(System.in);
-    public static void printMovieTitle(String movieTitle){
-        System.out.println("Movie is " + movieTitle);
-    }
+
     /**
      * Method to notify user about the input String
      * @param message info about input
@@ -23,5 +22,62 @@ public class utilF {
 
         return input;
     }
+
+    //Method to check the int value
+    public static int readIntInput(String Message) {
+        System.out.println(Message);
+        while (true) {
+            try {
+                String s = sc.next();
+                return Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                System.out.println("Please, input a valid decimal number. ");
+            }
+        }
+    }
+
+    /*
+     method to notify user the allowable range of seat input
+     */
+    public static int readSeatInput(String message, int min, int max) {
+        int c = 0;
+        do {
+            if(min == max)
+                readIntInput(message + " (" + max + "):");
+            else
+                c = readIntInput(message + " (" + min + "~" + max + "): ");
+            if (!(c >= min && c <= max))
+                System.out.println("Please enter valid option. ");
+        } while (!(c >= min && c <= max));
+        return c;
+    }
+
+    //Method function to print the menu
+    public static void printMenu(ArrayList<String> choices, int choiceIdFrom) {
+        for (String choice : choices) {
+            System.out.println(((choiceIdFrom++) + ": " + choice));
+            System.out.println();
+        }
+    }
+
+    /*
+     Confirmation Message:
+     This method only accept 'Yes' or 'No' as input
+     return true  if input is 'Yes' or
+     return false if input is 'No'
+     Otherwise it will repeatedly prompt user for input
+     */
+    public static boolean confirm(String message) {
+        while (true) {
+            String in = read(message + " (Yes/No): ");
+            if (in.equalsIgnoreCase("Yes"))
+                return true;
+            else if (in.equalsIgnoreCase("No"))
+                return false;
+        }
+    }
+
+
+
 
 }
